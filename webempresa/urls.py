c7669b53b5821cfb878d5b1c5f169135a19361df
Sergio Gabriel <sgrodriguez@gmail.com>
2018-09-20 09:16:45 -0300

    Separar el archivo de urls de la app core

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -14,16 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views as core_views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', core_views.home, name="home"),
-    path('about/', core_views.about, name="about"),
-    path('services/', core_views.services, name="services"),
-    path('store/', core_views.store, name="store"),
-    path('contact/', core_views.contact, name="contact"),
-    path('blog/', core_views.blog, name="blog"),
-    path('sample/', core_views.sample, name="sample"),
+    # Paths del core
+    path('', include('core.urls')),
+    # Paths del admin
     path('admin/', admin.site.urls),
 ]
