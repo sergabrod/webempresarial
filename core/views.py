14ca51c1b8f95a3a21bba7ce41e0f89c2e21daa1
Sergio Gabriel <sgrodriguez@gmail.com>
2018-09-25 12:48:01 -0300

    Creando el frontend, extendiendo del layout

--- core/views.py
+++ core/views.py
@@ -3,28 +3,28 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Home</h2>")
+   return render(request, "core/index.html", {'active_home': 'active'})
 
 
 def about(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>About</h2>")
+   return render(request, "core/about.html", {'active_about': 'active'})
 
 
 def services(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Services</h2>")
+   return render(request, "core/services.html", {'active_services': 'active'})
 
 
 def store(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Store</h2>")
+   return render(request, "core/store.html", {'active_store': 'active'})
 
 
 def contact(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Contact</h2>")
+   return render(request, "core/contact.html", {'active_contact': 'active'})
 
 
 def blog(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Blog</h2>")
+   return render(request, "core/blog.html", {'active_blog': 'active'})
 
 
 def sample(request):
-   return HttpResponse("<h1>Web Empresarial</h1><h2>Sample</h2>")
+   return render(request, "core/sample.html")
