848777eb746a2c0e1ee5228155aebed8d160107c
Sergio Gabriel <sgrodriguez@gmail.com>
2018-09-26 11:21:14 -0300

    Agregado al admin del modelo ServicesC

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views as core_views
+from django.conf import settings
 
 urlpatterns = [
     path('', core_views.home, name="home"),
@@ -10,3 +11,8 @@ urlpatterns = [
     path('blog/', core_views.blog, name="blog"),
     path('test/', core_views.sample, name="sample"),
 ]
+
+# Si estamos en modo DEBUG agregamos la ruta static de media a las urlpatterns
+if settings.DEBUG:
+   from django.conf.urls.static import static
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webempresa/settings.py
+++ webempresa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'Services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
