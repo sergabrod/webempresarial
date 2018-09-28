2f5eedd597512a9e0a03860744be99740542abe3
Sergio Gabriel <sgrodriguez@gmail.com>
2018-09-28 11:43:20 -0300

    Mostrar posts por categorías

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views as blogs_views
 
 urlpatterns = [
     path('', blogs_views.blog, name="blog"),
+    path('category/<int:category_id>/', blogs_views.category, name="category"),
 ]
